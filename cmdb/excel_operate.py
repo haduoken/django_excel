0997a11eeb54d948aed7900f6a77dd3f1f2828ed
yijie <zhangyj@kilox.cn>
2019-06-29 19:52:51 +0800

    add more

--- cmdb/excel_operate.py
+++ cmdb/excel_operate.py
@@ -4,38 +4,45 @@ import os
 import sys
 from openpyxl import load_workbook
 
+
 def get_visible_names(file):
-	wb = load_workbook(file)
-	visible_sheets = [sheet for sheet in wb.sheetnames if wb[sheet].sheet_state == 'visible']
-	return visible_sheets
+    wb = load_workbook(file)
+    visible_sheets = [sheet for sheet in wb.sheetnames if wb[sheet].sheet_state == 'visible']
+    return visible_sheets
+
 
 # 获取保留的属性
-def get_keys(file,sheet_name):
-	# out_file = os.path.join('excels','out.xlsx')
-	# out_sheet_name = '明细汇总'
-	# out_sheet_name = get_visible_names(out_file)
-	out_frame = pd.read_excel(file,sheet_name=sheet_name)
-	keys = out_frame.keys().str
-	keys1 = out_frame.keys().values
-	keys_out = keys1.tolist()
-	return keys_out
+def get_keys(file, sheet_name):
+    # out_file = os.path.join('excels','out.xlsx')
+    # out_sheet_name = '明细汇总'
+    # out_sheet_name = get_visible_names(out_file)
+    out_frame = pd.read_excel(file, sheet_name=sheet_name)
+    keys1 = out_frame.keys().values
+    keys_out = list(keys1.tolist())
+    # 过滤掉没有名字的
+    keys_out = [key for key in keys_out if not 'Unnamed:' in key]
+    print('get keys {}'.format(keys_out))
+    return keys_out
 
-def write_file_by_keys(keys):
-	# load other files
-	files = os.listdir('excels')
-	f_list=[]
-	out_frame = DataFrame()
-	for file in files:
-		if file=='out.xlsx':
-			continue
-		file_name = os.path.join('excels', file)
-		sheet_name = get_visible_names(file_name)
-		if isinstance(sheet_name,list):
-			# f = pd.read_excel(file_name,sheet_name=sheet_name)
-			for sheet in sheet_name:
-				f = pd.read_excel(file_name,sheet_name=sheet)
-				keys_have = f.keys().values.tolist()
-				keys_use = [key for key in keys_out if key in keys_have]
-				tmp = f[keys_use]
-				out_frame = pd.concat([out_frame,tmp],sort=False)
-	out_frame.to_excel('output.xlsx')
+
+def write_file_by_keys(files, store_keys):
+    out_frame = DataFrame(columns=store_keys)
+    for file in files:
+        sheet_name = get_visible_names(file)
+        if isinstance(sheet_name, list):
+            for sheet in sheet_name:
+                f = pd.read_excel(file, sheet_name=sheet)
+                keys_have = f.keys().values.tolist()
+                keys_use = [key for key in store_keys if key in keys_have]
+                tmp = f[keys_use]
+                out_frame = pd.concat([out_frame, tmp], sort=False)
+    output_excel = os.path.join('static', 'output.xlsx')
+    out_frame.to_excel(output_excel,columns=store_keys)
+    print('write file {} with keys {}'.format(output_excel,keys_use))
+
+def test_print_excel_keys(excel):
+    for s in get_visible_names(excel):
+        get_keys(excel,s)
+    
+if __name__ == '__main__':
+    test_print_excel_keys(sys.argv[1])
--- cmdb/views.py
+++ cmdb/views.py
@@ -2,43 +2,83 @@ from django.shortcuts import render
 from .excel_operate import *
 from django.core.files.storage import default_storage
 
-
 # Create your views here.
 from django.shortcuts import HttpResponse
-def index(request):
-	# return HttpResponse('Hello world')
-	# return render(request,'index.html')
-	# if request.method=="POST":
-	# 	username = request.POST.get("username",None)
-	# 	password = request.POST.get("password",None)
-	# 	print(username,password)
-	# image_file = request.POST.get("upload_file",None)
-	# if image_file:
-	# 	img = cv2.imread(image_file)
-	# 	cv2.imshow(img)
-	# 	cv2.waitkey(0)
-	# 只有在请求方法是POST，并且请求页面中 < form > 有enctype = "multipart/form-data"
-	# 属性时FILES才拥有数据。否则，FILES
-	# 是一个空字典。
-	upload_files = []
-	if request.method=="POST":
-		print(request.FILES['upload_file'])
-		for name,upload in request.FILES.items():
-			# print(upload)
-			# 这里以二进制打开，方便写文件
-			file_name = upload.name
-			with default_storage.open(file_name, 'wb') as f:
-				for chunk in upload.chunks():
-					f.write(chunk)
-				upload_files.append(file_name)
-	# 第一步解析用户上传的excel的keys
-	keys = []
-	for file in upload_files:
-		for sheet in get_visible_names(file):
-			keys.extend(get_keys(file,sheet))
-			keys = list(set(keys))
-	print(keys)
-	# 使用context传递信息
-	context = dict({'keys':keys})
-	return render(request,'index.html',context)
 
+upload_files = []
+upload_file_names =[]
+store_keys = []
+select_keys = []
+
+def index(request):
+    # return HttpResponse('Hello world')
+    # return render(request,'index.html')
+    # if request.method=="POST":
+    # 	username = request.POST.get("username",None)
+    # 	password = request.POST.get("password",None)
+    # 	print(username,password)
+    # image_file = request.POST.get("upload_file",None)
+    # if image_file:
+    # 	img = cv2.imread(image_file)
+    # 	cv2.imshow(img)
+    # 	cv2.waitkey(0)
+    # 只有在请求方法是POST，并且请求页面中 < form > 有enctype = "multipart/form-data"
+    # 属性时FILES才拥有数据。否则，FILES
+    # 是一个空字典。
+    # request.session['upload_files']=[]
+    # session_files=[]
+    # if request.session.has_key('upload_files'):
+    #     session_files = request.session['upload_files']
+    # session_keys=[]
+    # if request.session.has_key('store_keys'):
+    #     session_keys = request.session['store_keys']
+    done = False
+    
+    global store_keys
+    global upload_files
+    global upload_file_names
+    global select_keys
+    
+    if request.method == "POST":
+        post_message = request.POST
+        if post_message.get('insert',None):
+            print(request.FILES['upload_file'])
+            for name, upload in request.FILES.items():
+                # print(upload)
+                # 这里以二进制打开，方便写文件
+                file_name = upload.name
+                write_url = os.path.join('static', file_name)
+                with default_storage.open(write_url, 'wb') as f:
+                    for chunk in upload.chunks():
+                        f.write(chunk)
+                    upload_file_names.append(file_name)
+                    upload_files.append(write_url)
+                    for sheet in get_visible_names(write_url):
+                        store_keys = store_keys+get_keys(write_url,sheet)
+                        store_keys = list(set(store_keys))
+                    # if session_keys:
+                    #     session_keys.extend(get_keys(write_url, sheet))
+                    # request.session['store_keys']=store_keys
+        elif post_message.get('generate',None):
+            select_index = post_message.getlist('check_box_list',None)
+            tmp_select = [store_keys[int(index)] for index in select_index]
+            select_keys.extend(tmp_select)
+        elif post_message.get('done',None):
+            if select_keys:
+                write_file_by_keys(upload_files,select_keys)
+                done = True
+        elif post_message.get('clear',None):
+            for file in os.listdir('static'):
+                if '.xlsx' in file:
+                    os.remove(os.path.join('static',file))
+            upload_files.clear()
+            upload_file_names.clear()
+            store_keys.clear()
+            select_keys.clear()
+            
+    
+    # if request.method == 'POST' and request.POST.has_key('done'):
+    # 第一步解析用户上传的excel的keys
+    # 使用context传递信息
+    context = dict({'use_keys': select_keys,'valid_keys': store_keys,'done':done,'files':upload_file_names})
+    return render(request, 'index.html', context)
